13b334f6d504a584f9254b7bca903fec52a31f84
mateicrainiceanu <100783062+mateicrainiceanu@users.noreply.github.com>
2024-04-16 12:43:16 +0300

    Managed TextField Update with MUI component

--- src/components/FormElement.tsx
+++ src/components/FormElement.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import {formElement} from "@/types";
+import {TextField} from "@mui/material";
 
 interface Props {
 	name: string;
-    type: string;
-    label: string
+	type: string;
+	label: string;
 	handleChange: (e: any) => void;
 	value: string | number;
 }
 
 function FormElement({name, type, label, handleChange, value}: Props) {
 	return (
-		<>
-			<label className="" htmlFor={name}>{label}</label>
+		<div className="my-2">
+			<TextField
+				variant="filled"
+				label={label}
+				name={name}
+				type={type}
+				className="w-full bg-white rounded-lg "
+				value={value}
+				onChange={handleChange}></TextField>
+			{/* <label className="" htmlFor={name}>{label}</label>
 			<input
 				id={name}
 				name={name}
@@ -20,10 +29,9 @@ function FormElement({name, type, label, handleChange, value}: Props) {
 				type={type}
                 onChange={handleChange}
 				className="w-full my-2 h-8 py-4 px-5 text-black rounded-lg text-center"
-			/>
-		</>
+			/> */}
+		</div>
 	);
 }
 
 export default FormElement;
-
