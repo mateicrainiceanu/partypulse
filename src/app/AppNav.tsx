d62437617cfb671e1ec1c7d37a274209ad246c3d
mateicrainiceanu <100783062+mateicrainiceanu@users.noreply.github.com>
2024-05-07 21:30:24 +0300

    Incremental small changes

--- src/app/AppNav.tsx
+++ src/app/AppNav.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
 
 	return (
 		<>
-			<div className="flex justify-between items-center w-full h-20 px-4 text-white sticky top-0 my-2 nav ">
+			<div className="flex justify-between items-center w-full h-20 px-4 text-white sticky top-0 my-2 nav z-50">
 				<div>
 					<Link href="/">
 						<Image draggable={false} priority src="/logo_white.png" width="100" height="100" alt="logo" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import Image from "next/image";
-import { FaMusic } from "react-icons/fa";
+import {FaMusic} from "react-icons/fa";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center p-24">
-			<div className="min-h-screen flex items-center justify-center">
+			<div className="min-h-11/12 flex items-center justify-center">
 				<div>
-					<FaMusic className="text-7xl mx-auto mb-10"/>
+					<FaMusic className="text-7xl mx-auto mb-10" />
 					<h1 className="text-5xl font-sans leading-snug text-center">
 						Welcome to <b className="font-bold">PartyPulse!</b>
 					</h1>
-          <h2 className="text-center font-mono text-gray-300">An app for your fun and partys</h2>
-          
+					<h2 className="text-center font-mono text-gray-300">An app for your fun and partys</h2>
 				</div>
 			</div>
-
 		</main>
 	);
 }
